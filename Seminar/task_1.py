2ef5cd6402903ed61b1941fe462694df1f8a9735
«Elmir» <elmir090890@mail.ru>
2023-04-16 11:25:42 +0300

    Commit for HW Task 12

---
+++ Seminar/task_1.py
@@ -0,0 +1,17 @@
+# По данному целому неотрицательному n вычислите значение n!. 
+# N! = 1 * 2 * 3 * … * N (произведение всех чисел от 1 до N)
+# 0! = 1 Решить задачу используя цикл while
+
+# Input:       5
+
+# Output:    120
+
+number = int(input('Введите число: '))
+
+value = factorial = 1
+
+while value <= number:
+    factorial *= value
+    value += 1
+
+print(f'Факториал {number} = {factorial}')
